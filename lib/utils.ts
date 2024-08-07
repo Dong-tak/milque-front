61c09e2b2a5d32513da5d5e77ec6a119532c3371
profitG <dldbsry55@gmail.com>
2024-08-07 16:24:49 +0900

    ✨ Add: shadcn 설치
    ## ✅ What I did
    - tailwind.config 다 갈렸고
    - global.css도 다 갈림
    - 테스트 용으로 branch 만들어서 진행
    
    ## #️⃣ Linked issues

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,6 @@
-export function formatToDate(date: string) {
-  const dayInMs = 1000 * 60 * 60 * 24;
-  const time = new Date(date).getTime();
-  const now = new Date().getTime();
-  const diff = Math.round((time - now) / dayInMs);
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-  const formatter = new Intl.RelativeTimeFormat("ko");
-  return formatter.format(diff, "days");
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
