384ad00311e15cb499149689ab1fe4d656095136
Dong-tak <lonu68325@gmail.com>
2024-08-16 22:53:49 +0900

    💄 Improve: feat: layout.tsx에 sidebar 추가 및 뒤로가기 버튼 조건 수정
    ## ✅ What I did
    
    ## #️⃣ Linked issues

--- components/our-option.tsx
+++ components/our-option.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import {
   Dialog,
   DialogTrigger,
@@ -10,66 +12,70 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { OurOptionSidebar } from "./our-optionsidebar";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+
+const DialogClose = DialogPrimitive.Close;
 
 export function OurOption() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
+        <Button variant="outline" onClick={() => setIsOpen(true)}>
+          옵션설정
+        </Button>
       </DialogTrigger>
-      <div className="relative flex flex-col items-center justify-center px-[72px] py-[24px]">
-        <DialogContent className="flex items-center justify-center">
-          <div className="h-full xl:w-[250px]">
-            {/*사이드바 - 마일퀘설정하기*/}
-          </div>
-          {/*Content view*/}
-          <div className="relative flex h-[743px] w-[636px] items-center justify-center">
-            <div className="flex h-full w-full flex-col items-center gap-6 p-8">
-              <DialogHeader className="h-auto w-full items-start gap-2 border-b py-4">
-                <DialogTitle className="font-['SUIT Variable'] h-auto w-full text-xl font-bold leading-7 text-slate-900">
-                  프로필 설정
-                </DialogTitle>
-                <DialogDescription className="font-['SUIT Variable'] h-auto text-sm font-normal leading-tight text-slate-400">
-                  친구들에게 보여지는 나의 정보입니다.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="flex w-full flex-col gap-6">
-                <div className="w-full grid-cols-4 flex-col items-center gap-4">
-                  <Label htmlFor="name" className="text-right">
-                    닉네임
-                  </Label>
-                  <Input
-                    id="name"
-                    value="Pedro Duarte"
-                    className="col-span-3 w-full"
-                  />
-                  <DialogDescription className="font-['SUIT Variable'] h-auto text-sm font-normal leading-tight text-slate-400">
-                    닉네임은 한 번 설정하면 수정이 어렵습니다. 해당 계정으로
-                    이메일도 만들어집니다.
-                  </DialogDescription>
-                </div>
-                <div className="w-full flex-col items-center gap-4">
-                  <Label htmlFor="username" className="text-right">
-                    비밀번호 재설정
-                  </Label>
-                  <Input
-                    id="username"
-                    value="@peduarte"
-                    className="col-span-3"
-                  />
-                </div>
-              </div>
-              <DialogFooter className="flex">
-                <Button type="submit">초기화</Button>
-                <Button type="submit">수정하기</Button>
-              </DialogFooter>
+      {isOpen && (
+        <div className="fixed inset-0 z-40 flex h-screen items-center justify-center bg-[#1E293B] bg-opacity-50 px-[72px] py-[24px]">
+          <DialogContent className="flex h-auto max-w-[1200px] flex-shrink flex-grow items-start justify-center bg-white shadow-lg">
+            {/* Side bar */}
+            <div className="flex h-[743px] w-[250px] flex-shrink-0 items-center justify-center border-r">
+              <OurOptionSidebar />
             </div>
-          </div>
-          <div className="h-full xl:w-[250px]">
-            {/* 우측 빈공간 - 브레이크 포인트 1100px 보다 작으면 없어짐 */}
-          </div>
-        </DialogContent>
-      </div>
+            {/* Content view */}
+            <div className="relative flex h-[743px] min-h-0 w-[636px] min-w-0 flex-grow items-center justify-center">
+              <div className="flex h-full w-full flex-col items-center gap-6 p-8">
+                <DialogHeader className="h-auto w-full items-start gap-2 border-b py-4">
+                  <DialogTitle className="font-['SUIT Variable'] h-auto w-full text-xl font-bold leading-7 text-slate-900">
+                    프로필 설정
+                  </DialogTitle>
+                  <DialogDescription className="font-['SUIT Variable'] h-auto text-sm font-normal leading-tight text-slate-400">
+                    친구들에게 보여지는 나의 정보입니다.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="flex w-full flex-col gap-6">
+                  <div className="w-full flex-col items-center space-y-2">
+                    <Label htmlFor="name" className="text-lift">
+                      닉네임
+                    </Label>
+                    <Input id="name" value="Pedro Duarte" className="w-full" />
+                    <DialogDescription className="font-['SUIT Variable'] h-auto text-sm font-normal leading-tight text-slate-400">
+                      닉네임은 한 번 설정하면 수정이 어렵습니다. 해당 계정으로
+                      이메일도 만들어집니다.
+                    </DialogDescription>
+                  </div>
+                  <div className="w-full flex-col items-center space-y-2">
+                    <Label htmlFor="username" className="text-lift">
+                      프로필 사진
+                    </Label>
+                    <Input id="username" value="@peduarte" className="w-full" />
+                  </div>
+                </div>
+                <DialogFooter className="flex">
+                  <Button type="submit">초기화</Button>
+                  <Button type="submit">수정하기</Button>
+                </DialogFooter>
+              </div>
+            </div>
+            <div className="hidden h-full xl:block xl:w-[250px]">
+              {/* 우측 빈공간 - 브레이크 포인트 1100px 보다 작으면 없어짐 */}
+            </div>
+          </DialogContent>
+        </div>
+      )}
     </Dialog>
   );
 }
---
+++ components/our-optionsidebar.tsx
@@ -0,0 +1,165 @@
+import {
+  Link,
+  LogIn,
+  PanelLeftClose,
+  FileUp,
+  Info,
+  MessageSquare,
+  ThumbsUp,
+  Bold,
+  PartyPopper,
+  LayoutTemplate,
+  Sidebar,
+  Share2,
+  Network,
+  User,
+  Bell,
+  DownloadCloud,
+  Ghost,
+} from "lucide-react";
+import { Command as CommandPrimitive } from "cmdk";
+
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { SelectGroup, SelectLabel } from "@radix-ui/react-select";
+import { OurAvatar } from "./our-avatar";
+
+// const SidebarItem = React.forwardRef<
+//   React.ElementRef<typeof CommandPrimitive.Item>,
+//   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Item>
+// >(({ className, ...props }, ref) => (
+//   <CommandPrimitive.Item
+//     ref={ref}
+//     className={cn(
+//       "body-normal-body-01 relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 outline-none data-[disabled=true]:pointer-events-none data-[selected='true']:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50",
+//       className,
+//     )}
+//     {...props}
+//   />
+// ));
+
+interface SidebarItemProps {
+  children: React.ReactNode;
+  disabled?: boolean;
+}
+
+function SidebarItem({ children, disabled }: SidebarItemProps) {
+  return (
+    <CommandItem disabled={disabled} className="px-4 py-2 others-medium-button">
+      {children}
+    </CommandItem>
+  );
+}
+
+function SidebarList({ children }: { children: React.ReactNode }) {
+  return <CommandList className="max-h-none">{children}</CommandList>;
+}
+
+export function OurOptionSidebar() {
+  return (
+    <Command className="flex h-[743px] min-w-[250px] items-center justify-center">
+      <SidebarList>
+        <SelectGroup className="flex h-full w-full items-center justify-center p-2">
+          <div className="flex h-auto w-full items-center justify-start px-4 py-2">
+            <SelectLabel className="font-['SUIT Variable'] items-start text-left text-xl font-bold leading-7 text-black">
+              마일퀘 설정하기
+            </SelectLabel>
+            <div className="h-auto w-auto"></div>
+            <div className="p-3">
+              <PanelLeftClose className="relative h-5 w-5 items-end" />
+            </div>
+          </div>
+        </SelectGroup>
+        <CommandGroup>
+          <SidebarItem>
+            <Ghost className="mr-2 h-4 w-4" />
+            <span>프로필 설정</span>
+          </SidebarItem>
+          <SidebarItem>
+            <Link className="mr-2 h-4 w-4" />
+            <span>소셜 계정 설정</span>
+          </SidebarItem>
+          <SidebarItem>
+            <DownloadCloud className="mr-2 h-4 w-4" />
+            <span>다운로드 설정</span>
+          </SidebarItem>
+          <SidebarItem>
+            <Bell className="mr-2 h-4 w-4" />
+            <span>알림 설정</span>
+          </SidebarItem>
+        </CommandGroup>
+        <SelectGroup className="pl-5 pt-4"></SelectGroup>
+        <CommandGroup>
+          <SidebarItem>
+            <User className="mr-2 size-4" />
+            <span>친구설정</span>
+          </SidebarItem>
+          <SidebarItem disabled>
+            <Network className="mr-2 h-4 w-4" />
+            <span>그룹설정</span>
+            <CommandShortcut>준비중</CommandShortcut>
+          </SidebarItem>
+          <SidebarItem>
+            <Share2 className="mr-2 h-4 w-4" />
+            <span>공유 설정</span>
+          </SidebarItem>
+        </CommandGroup>
+        <CommandGroup>
+          <SidebarItem>
+            <Sidebar className="mr-2 size-4" />
+            <span>화면 설정</span>
+          </SidebarItem>
+          <SidebarItem>
+            <LayoutTemplate className="mr-2 h-4 w-4" />
+            <span>템플릿 설정</span>
+          </SidebarItem>
+          <SidebarItem>
+            <PartyPopper className="mr-2 h-4 w-4" />
+            <span>이벤트</span>
+            <CommandShortcut>24</CommandShortcut>
+          </SidebarItem>
+          <SidebarItem>
+            <Bold className="mr-2 h-4 w-4" />
+            <span>베타 테스트 신청</span>
+            <CommandShortcut>대기중</CommandShortcut>
+          </SidebarItem>
+          <SidebarItem>
+            <ThumbsUp className="mr-2 h-4 w-4" />
+            <span>무료 팀기능 도입 문의</span>
+          </SidebarItem>
+          <SidebarItem>
+            <MessageSquare className="mr-2 h-4 w-4" />
+            <span>불편/건의 사항</span>
+          </SidebarItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup>
+          <SidebarItem>
+            <Info className="mr-2 size-4" />
+            <span>도움말</span>
+          </SidebarItem>
+          <SidebarItem>
+            <FileUp className="mr-2 h-4 w-4" />
+            <span>업데이트 노트</span>
+            <CommandShortcut>V1.0.3</CommandShortcut>
+          </SidebarItem>
+          <SidebarItem>
+            <LogIn className="mr-2 h-4 w-4" />
+            <span>로그아웃</span>
+          </SidebarItem>
+        </CommandGroup>
+      </SidebarList>
+    </Command>
+  );
+}
