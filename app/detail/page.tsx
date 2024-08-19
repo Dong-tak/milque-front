e0daff9916dd6f56ebf84797f49a0bf3e00a2395
profitG <dldbsry55@gmail.com>
2024-08-19 16:45:10 +0900

    : Remove unused code files and components
    ## ✅ What I did
    
    ## #️⃣ Linked issues

--- app/detail/page.tsx
+++ app/detail/page.tsx
@@ -1,79 +1,9 @@
-import DetailBtmNav from "@/components/detail-btm-nav";
-import DetailComment from "@/components/detail-comment";
-import { DetailTopNav } from "@/components/detail-top-nav";
-import InstagramFeedEmbed from "@/components/insta-feed";
-import InstagramReelsEmbed from "@/components/insta-reels";
-import TiktokEmbed from "@/components/tiktok-embed";
-import { Button } from "@/components/ui/button";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import Image from "next/image";
-import { useState } from "react";
-import { tiktokContent } from "../team/page";
-import YouTubeEmbed from "@/components/youtube-video";
-import YouTubeShortsEmbed from "@/components/youtube-shorts";
+import DetailPage from "@/components/detail-page";
 
-interface DetailPageProps {
-  form: "instagram" | "youtubeVid" | "youtubeSht" | "tiktok";
-}
-
-export default function DetailPage({ form }: DetailPageProps) {
-  let backgroundColor = "";
-  let itemsArray = "items-start";
-  let justifyArray = "justify-end";
-  let width = "w-full";
-
-  if (form === "youtubeVid") {
-    backgroundColor = "bg-card";
-    itemsArray = "items-center";
-    justifyArray = "justify-center";
-  } else if (form === "tiktok") {
-    backgroundColor = "bg-none";
-    itemsArray = "items-center";
-    justifyArray = "";
-    width = "w-auto";
-  }
-
+export default function Team() {
   return (
-    <div className="flex h-screen items-center justify-center bg-neutral-500 py-6">
-      <div className="hidden md:block">
-        <Button variant="outline" size={"icon"} className="mx-5">
-          <ChevronLeft className="size-4" />
-        </Button>
-      </div>
-      <div className="flex w-full max-w-[1200px] items-center justify-center">
-        <div
-          className={`hidden h-screen max-h-[785px] ${width} max-w-[514px] py-6 md:block`}
-        >
-          <div
-            className={`flex h-full w-auto rounded-l-md ${backgroundColor} ${itemsArray} ${justifyArray} overflow-hidden`}
-          >
-            {/* <InstagramFeedEmbed
-              url={"https://www.instagram.com/p/C-dNdiKMYfY/"}
-            /> */}
-            {/* <InstagramReelsEmbed
-              url={
-                "https://www.instagram.com/reel/C9mPBPSJwGt/?utm_source=ig_web_copy_link/"
-              }
-            /> */}
-            {/* <InstagramFeedEmbed
-              url={
-                "https://www.instagram.com/p/C8333eatFmY/?utm_source=ig_web_copy_link"
-              }
-            /> */}
-            <TiktokEmbed content={tiktokContent} />
-            {/* <YouTubeEmbed url={"5JafqFjBnBU"} /> */}
-            {/* <YouTubeShortsEmbed url={"q4IkcMIk70M"} /> */}
-          </div>
-        </div>
-        <div className="flex w-auto md:min-w-0">
-          <DetailComment />
-        </div>
-      </div>
-      <div className="hidden md:block">
-        <Button variant="outline" size={"icon"} className="mx-5">
-          <ChevronRight className="size-4" />
-        </Button>
-      </div>
+    <div>
+      <DetailPage form="instagram" />
     </div>
   );
 }
--- app/team/list/page.tsx
+++
@@ -1,9 +0,0 @@
-import DetailPage from "@/app/detail/page";
-
-export default function Team() {
-  return (
-    <div>
-      <DetailPage form="tiktok" />
-    </div>
-  );
-}
