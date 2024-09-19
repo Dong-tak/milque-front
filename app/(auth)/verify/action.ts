f432023d6c332e965cc85b7cb17dbdf4a2650edf
profitG <dldbsry55@gmail.com>
2024-09-20 08:32:01 +0900

    🔨 Refactor: login, signup data fetch refactoring
    ## ✅ What I did
    
    ## #️⃣ Linked issues

--- app/(auth)/verify/action.ts
+++ app/(auth)/verify/action.ts
@@ -1,6 +1,6 @@
 "use server";
 import dotenv from "dotenv";
-import { registerUser, verifyEmail } from "@/app/api/utils/api";
+import { verifyEmail } from "@/app/api/utils/api";
 
 dotenv.config();
 
@@ -16,32 +16,3 @@ export async function handleVerifyEmail(code: string) {
 }
 
 handleVerifyEmail("example_code");
-
-export async function ourverifyEmail(
-  code: string,
-): Promise<{ success: boolean; message?: string; error?: string }> {
-  try {
-    const response = await fetch(
-      `${POST_API_URL}/user/verify_email/`, // 환경 변수를 사용하여 URL 설정
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify({ code }),
-      },
-    );
-
-    if (response.ok) {
-      const data = await response.json();
-      return { success: true, message: data.message };
-    } else {
-      const errorData = await response.json();
-      return { success: false, error: errorData.error };
-    }
-  } catch (error) {
-    console.error("Error in verifyEmail:", error);
-    throw error; // 오류를 다시 던져서 handleVerifyEmail 함수로 전파
-  }
-}
--- app/api/utils/api.ts
+++ app/api/utils/api.ts
@@ -1,44 +1,4 @@
-import axios from "axios";
-// Register function
-export async function registerUser(
-  email: string,
-): Promise<{ success: boolean; message: string }> {
-  try {
-    // const csrfToken = getCsrfToken();
-    // console.log(csrfToken);
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_POST_API_URL}/user/verify_email/`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          // "X-CSRFToken": csrfToken || "", // Include the CSRF token in the headers
-        },
-        body: JSON.stringify({ email }),
-        credentials: "include", // Important: to send cookies (CSRF cookie)
-      },
-    );
-
-    if (response.ok) {
-      const data = await response.json();
-      return { success: true, message: data.message };
-    } else {
-      const errorData = await response.json();
-      return { success: false, message: errorData.error };
-    }
-  } catch (error) {
-    return {
-      success: false,
-      message: (error as Error).message || "Registration failed",
-    };
-  }
-}
-
-interface ApiResponse {
-  success: boolean;
-  message: string;
-  error?: string;
-}
+import { DataFetchInClient } from "@/components/postdata-client";
 
 // Verify Email function
 export async function verifyEmail(code: string): Promise<{
@@ -46,41 +6,8 @@ export async function verifyEmail(code: string): Promise<{
   success: boolean;
   message: string;
 }> {
-  try {
-    // Get the CSRF token from the cookie
-
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_POST_API_URL}/user/verify_email/`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ code }),
-        credentials: "include", // Important: to send cookies (CSRF cookie)
-      },
-    );
-
-    if (response.ok) {
-      const data = await response.json();
-      return {
-        success: true,
-        message: data.message,
-        error: undefined, // success인 경우 error는 undefined로 설정
-      };
-    } else {
-      const errorData = await response.json();
-      return {
-        success: false,
-        message: errorData.message,
-        error: errorData.error || "Unknown error",
-      };
-    }
-  } catch (error) {
-    return {
-      success: false,
-      message: (error as Error).message,
-      error: (error as Error).message || "Unknown error", // error가 없으면 기본값 설정
-    };
-  }
+  const apiUrl = `${process.env.NEXT_PUBLIC_POST_API_URL}/user/verify_email/`;
+  const bodyData = { code };
+  const data = await DataFetchInClient({ apiUrl, bodyData });
+  return data;
 }
