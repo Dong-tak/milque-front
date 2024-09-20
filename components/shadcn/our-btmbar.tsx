df9198de9efdf97fcaf0d01e3cfa76ccace49035
profitG <dldbsry55@gmail.com>
2024-09-20 14:34:33 +0900

    🔨 Refactor: Refactor navigation button component structure and add support for active state
    ## ✅ What I did
    
    ## #️⃣ Linked issues

--- components/shadcn/our-btmbar.tsx
+++ components/shadcn/our-btmbar.tsx
@@ -2,60 +2,16 @@
 
 import {
   ArrowLeft,
-  Bell,
-  Bookmark,
-  Calculator,
   Calendar,
-  ChevronRight,
   Compass,
-  CreditCard,
-  FileDown,
   Ghost,
   HeartHandshake,
-  History,
-  Link,
-  LogIn,
-  LogOut,
   Pin,
-  Plus,
-  Scan,
-  Settings,
-  Smile,
   SquarePlus,
-  User,
-  UserPlus,
-  Users,
 } from "lucide-react";
-import { Command as CommandPrimitive } from "cmdk";
-import cookie from "cookie";
 
-import {
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-  CommandShortcut,
-} from "@/components/ui/command";
 import React, { useState } from "react";
-import { cn } from "@/lib/utils";
-import { SelectGroup, SelectLabel } from "@radix-ui/react-select";
-import { OurAvatar } from "./our-avatar";
-import { usePathname, useRouter } from "next/navigation";
-import { Badge, SqBadge } from "../ui/badge";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-} from "@radix-ui/react-dropdown-menu";
+import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
 import {
   Dialog,
@@ -66,70 +22,12 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog";
-import { SidebarBtn } from "./our-sidebar";
 import { Input } from "../ui/input";
-
-interface SidebarItemProps {
-  children: React.ReactNode;
-  className?: string;
-  disabled?: boolean;
-  onSelect?: () => void;
-}
-
-function SidebarItem({
-  className,
-  children,
-  disabled,
-  onSelect,
-}: SidebarItemProps) {
-  return (
-    <CommandItem
-      onSelect={onSelect}
-      disabled={disabled}
-      className={cn("p-[15px] others-medium-button", className)}
-    >
-      {children}
-    </CommandItem>
-  );
-}
-
-function SidebarList({ children }: { children: React.ReactNode }) {
-  return (
-    <CommandList className="flex max-h-none flex-row">{children}</CommandList>
-  );
-}
-
-const NavButton = React.forwardRef<
-  HTMLButtonElement,
-  {
-    children: React.ReactNode;
-    onClick?: () => void;
-    disabled?: boolean;
-    isActive?: boolean;
-    className?: string;
-  }
->(({ children, onClick, disabled, isActive = false, className }, ref) => {
-  return (
-    <Button
-      variant="background"
-      size={"nav"}
-      disabled={disabled}
-      className={cn(
-        "w-full border-none hover:text-accent-foreground",
-        isActive && "text-accent-foreground",
-        className,
-      )}
-      onClick={onClick}
-      ref={ref}
-    >
-      {children}
-    </Button>
-  );
-});
-NavButton.displayName = "NavButton";
+import { DataFetchInClient } from "@/app/api/postdata-client";
+import { RoutePage } from "../route-setting";
+import { NavButton } from "./navigation-btn";
 
 export function OurBtmBar({ user_id }: { user_id: string }) {
-  const router = useRouter();
   const pathname = usePathname();
   const [contentUrl, setContentUrl] = useState("");
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -137,74 +35,25 @@ export function OurBtmBar({ user_id }: { user_id: string }) {
   };
 
   const handleSaveClick = async () => {
-    try {
-      const cookies = cookie.parse(document.cookie);
-      const accessToken = cookies.accessToken;
-      console.log(accessToken);
-
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_POST_API_URL}/feed/${user_id}/create/`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-          },
-          body: JSON.stringify([
-            {
-              content_url: contentUrl,
-              comment: "Comment를 입력하세요.",
-            },
-          ]),
-        },
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to save the content");
-      }
-
-      const data = await response.json();
-      console.log("Data saved successfully:", data);
-      window.location.reload();
-    } catch (error) {
-      console.error("Error saving data:", error);
-    }
-  };
-
-  const navToHome = () => {
-    router.push(`/home/${user_id}`);
-  };
-  const navToAdd = () => {
-    router.push("/add");
-  };
-  const navToGroup = () => {
-    router.push("/group");
-  };
-  const navToProfile = () => {
-    router.push("/profile");
-  };
-  const navToSetting = () => {
-    router.push("/setting");
-  };
-  const navToLink = () => {
-    router.push("/link");
-  };
-  const navToInvite = () => {
-    router.push("/invite");
-  };
-  const navToDownload = () => {
-    router.push("/download");
-  };
-  const navToMark = () => {
-    router.push("/bookmark");
+    const apiUrl = `${process.env.NEXT_PUBLIC_POST_API_URL}/feed/${user_id}/create/`;
+    const bodyData = [
+      {
+        content_url: contentUrl,
+        comment: "Comment를 입력하세요.",
+      },
+    ];
+    const data = await DataFetchInClient({ apiUrl, bodyData, isReload: true });
   };
 
   return (
     <div className="btm-bar fixed bottom-0 z-50 flex w-full border-t bg-background">
-      <NavButton onClick={navToHome} isActive={pathname === "/"}>
+      <NavButton
+        onClick={RoutePage(`/home/${user_id}`)}
+        isActive={pathname === "/"}
+      >
         <Calendar className="size-6" />
       </NavButton>
-      <NavButton onClick={navToHome} disabled>
+      <NavButton onClick={RoutePage(`/home/${user_id}`)} disabled>
         <Compass className="size-6" />
       </NavButton>
       <Dialog>
@@ -249,10 +98,16 @@ export function OurBtmBar({ user_id }: { user_id: string }) {
           </DialogFooter>
         </DialogContent>
       </Dialog>
-      <NavButton onClick={navToGroup} isActive={pathname === "/group"}>
+      <NavButton
+        onClick={RoutePage(`/home/${user_id}`)}
+        isActive={pathname === "/group"}
+      >
         <HeartHandshake className="size-6" />
       </NavButton>
-      <NavButton onClick={navToProfile} isActive={pathname === "/profile"}>
+      <NavButton
+        onClick={RoutePage(`/home/${user_id}`)}
+        isActive={pathname === "/profile"}
+      >
         <Ghost className="size-6" />
       </NavButton>
     </div>
