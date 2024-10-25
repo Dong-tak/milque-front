102d17b6e3d9446d3dc17175e894e8eee90da1d9
profitG <dldbsry55@gmail.com>
2024-10-25 14:34:20 +0900

    ✨ Add: record view 사이드바 기본 틀
    ## ✅ What I did
    
    ## #️⃣ Linked issues

---
+++ app/miro/recordview/page.tsx
@@ -0,0 +1,12 @@
+// app/board/page.tsx
+"use client";
+
+import React from "react";
+
+export default function BoardPage() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <h1>Record View</h1>
+    </div>
+  );
+}
